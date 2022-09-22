592145de3e63961178d94bba7598ef9cb6e753e0
Alberto Marques Nunes <alberto.nunes@primesolutions.com.br>
2022-09-22 20:18:06 -0300

    Database Seeding

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,24 @@
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Ana Paula Garcia','880.400.010-48','12.00','1972-08-17 9:09:08.545522','0');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Neyde Pestana','795.965.400-09','65535.00','1935-05-13 19:39:08.543222','4');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Alberto Nunes','333.890.430-47','16635.00','1968-02-11 8:50:08.543222','2');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Amelia Soane','342.110.650-92','8096.00','1930-07-10 19:39:08.543222','5');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Andrea Salgado','385.353.820-70','2048.00','1979-11-03 19:39:08.543222','0');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Elisabeth Salgado','487.747.390-42','1024.00','1945-02-08 19:39:08.543222','2');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Carlos Alberto Gil','249.231.750-10','512.00','1992-08-24 19:39:08.543222','2');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Luiz Roberto Gil','894.719.720-35','128.00','2000-03-12 19:39:08.543222','0');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Roberto Pestana','781.497.591-31','64.00','1965-07-25 19:39:08.543222','4');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Luiz Fernando Pestana','479.594.311-75','32.00','1966-11-01 19:39:08.543222','1');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Leandro Irineu Filho','601.242.865-00','16.00','1978-01-26 19:39:08.543222','3');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Arthur Gimenes','236.455.265-60','8.00','1973-06-06 19:39:08.543222','8');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Marcelo da Siva Marques','979.984.819-94','4096.00','1965-07-21 19:39:08.543222','3');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Wilson Nigro','544.802.901-92','10212.00','1967-09-15 19:39:08.543222','5');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Donato Gomes','996.505.741-90','512.00','1963-10-30 19:39:08.543222','2');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Jose Gomes','249.639.942-16','256.00','1968-02-29 19:39:08.543222','1');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Maria Luiza Pestana','772.182.332-36','199.00','1982-08-23 19:39:08.543222','0');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Paulo Marangoni Perazzo','372.470.532-83','133.00','1934-05-16 19:39:08.543222','1');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Mariano Godoi','795.938.908-07','1692.00','1966-09-22 19:39:08.543222','3');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Sinval dos Santos','669.039.238-45','136.00','1975-08-01 19:39:08.543222','2');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Jeronimo Lucena','196.656.318-37','112.00','1964-05-01 19:39:08.543222','2');
+INSERT INTO tb_client (name, cpf,income,birth_date,children) VALUES ('Andressa da Silva','315.896.984-01','142.00','1986-04-22 19:39:08.543222','0');
+
+
